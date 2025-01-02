f9d207205a7a3a99e0947dda5f41efd7a02cf2cb
Félix de Miguel Villalba <fdv1004@alu.ubu.es>
2025-01-02 13:01:54 +0100

    .

--- src/commodityMarket/agent/RunMarketAndFiveAgents.java
+++ src/commodityMarket/agent/RunMarketAndFiveAgents.java
@@ -26,7 +26,7 @@ public class RunMarketAndFiveAgents {
 		String logfolderPath = "log/" + dateString;
 		
 		//Set the duration of the negotiations to 30 seconds.
-		int negotiationLength = 5*1000; //5 seconds.
+		int negotiationLength = 30*1000;
 		
 		
 		//Run an instance of the Commodity Market
